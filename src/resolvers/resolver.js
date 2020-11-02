2e07ba9f5449cf68ee97c4c87add801372d86e7e
Craig_Donavin <crdonavin@gmail.com>
2020-11-01 22:58:43 -0600

    add query resolvers

--- src/resolvers/resolver.js
+++ src/resolvers/resolver.js
@@ -2,3 +2,29 @@ const bcrypt = require('bcryptjs')
 const jsonwebtoken = require('jsonwebtoken')
 const models = require('../models')
 require('dotenv').config()
+
+const resolvers = {
+    Query: {
+        async me(_, args, { user }) {
+            if(!user) throw new Error("You are not authenticated")
+            return await models.User.findBYPk(user.id)
+        },
+        async user(root, { id }, { user }) {
+            try {
+                if(!user) throw new Error("You are not authenticated")
+                return models.User.findBYPk(id)
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        },
+        async allUsers(root, args, { user }) {
+            try {
+                if (!user) throw new Error("You are not authenticated")
+                return models.User.findall()
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        }
+    }
+    
+}
